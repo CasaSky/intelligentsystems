b611e99e613d9ea0f57bcf64e26f6120b6964330
Michel Brüger <michel.brueger@haw-hamburg.de>
2019-01-09 21:19:39 +0100

    pattern1File pattern2File und wer weiss was noch

---
+++ src/main/java/learning/dbscan/WriteFile.java
@@ -0,0 +1,33 @@
+//package learning.dbscan;
+//
+//import java.io.*;
+//import java.util.List;
+//
+//class WriteFile
+//{
+//    public static void main(String[] args) throws IOException {
+//
+//    }
+//
+//    public static void writeFile(List<Point> dataSet, String filename) throws IOException{
+//        FileWriter fw = new FileWriter(""+filename+".data");  //.txt?
+//        BufferedWriter bw = new BufferedWriter(fw);
+//
+////        Patterngenerator patterngenerator = new Patterngenerator();
+////        Patterns patterns = new Patterns();
+//
+////        List<Point> pattern1Dataset = patterngenerator.generatePattern(patterns.createPattern1(), 1.0);
+//        for(Point point : dataSet){
+//            bw.write("" + point.getCoordinates().get(0) + "," + point.getCoordinates().get(1));
+//            bw.newLine();
+//        }
+//
+//        bw.close();
+//    }
+//}
+
+//        bw.write("test test test");
+//        bw.newLine();
+//        bw.write("tset tset tset");
+//
+//        bw.close();
